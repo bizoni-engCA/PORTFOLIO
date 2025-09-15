a76239ec7d099952293d27d801a8da9c5064f2ab
bizoni <jplbizoni@gmail.com>
2025-09-15 14:56:01 -0300

    "Aula 1 de c++ e algumas mudanças no terminal_LP"

---
+++ aula1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+class Person{
+    char nome[20];
+    int id;
+public:
+     void getdateils(){}
+
+};
+
+int main(){
+    Person p1;
+    return 0;
+}
